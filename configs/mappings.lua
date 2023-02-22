3e63e591e8c90031c12aacd640d67dacd3280593
andreciobezerra <andreciobezerra@gmail.com>
2023-02-21 23:34:50 -0300

    initial mappings

--- configs/mappings.lua
+++ configs/mappings.lua
@@ -2,4 +2,12 @@ local keymap = vim.api.nvim_set_keymap
 local default_opts = { noremap = true, silent = true }
 local expr_opts = { noremap = true, expr = true, silent = true }
 
-keymap("i", "<C-f>", Telescope fd)
+-- INSERT MODE
+keymap("i", "<C-f>", "<ESC>:Telescope fd<CR>", default_opts)
+keymap("i","<C-t>", "<ESC>:FloatermNew<CR>i", default_opts)
+keymap("i","<C-w>", "<ESC>:bd<CR>", default_opts)
+keymap("i","<C-S-Down>", "<ESC>:t.<CR>", default_opts)
+
+-- NORMAL MODE
+keymap("n", "<C-f>", ":Telescope fd<CR>", default_opts)
+keymap("n", "<C-w>", ":bd<CR>", default_opts)
--- init.lua
+++ init.lua
@@ -1,2 +1,4 @@
 require("configs.options")
 require("configs.plugins")
+require("configs.plugins-setups")
+require("configs.mappings")
